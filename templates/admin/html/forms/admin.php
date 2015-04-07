573e8b6657f473f225b7bab0ccd5c2aaf8a1f1ba
volter9 <volter925@gmail.com>
2015-04-06 19:19:27 -0700

    New experimental admin modify page view

--- templates/admin/html/forms/admin.php
+++ templates/admin/html/forms/admin.php
@@ -1,26 +1,34 @@
 <form action="<?php echo $scheme['action'] ?>" method="POST">
     <?php foreach ($scheme['form'] as $field => $type): ?> 
-        <label>
-            <?php if (isset($data['errors'][$field])): ?> 
-            <p class="error">
-                <?php echo $data['errors'][$field] ?> 
-            </p>
-            <?php endif; ?> 
+        <div class="clearfix">
+            <div class="clearfix">
+                <div class="left">
+                    <label for="form_<?php echo $field ?>">
+                        <div class="field-name">
+                            <?php echo $data['field'][$field] ?> 
+                        </div>
+                    </label>
+                </div>
             
-            <?php build_element(
-                $type, array_merge(['name' => $field], pluck($data, $field))
-            ) ?> 
-            
-            <div class="field-name">
-                <?php echo $data['field'][$field] ?> 
+                <div class="right">
+                    <?php build_element(
+                        $type, array_merge(['name' => $field], pluck($data, $field))
+                    ) ?> 
+                    
+                    <?php if (isset($data['errors'][$field])): ?> 
+                    <p class="error">
+                        <?php echo $data['errors'][$field] ?> 
+                    </p>
+                    <?php endif; ?> 
+                    
+                    <?php if (isset($data['tooltip'][$field])): ?> 
+                    <div class="tooltip">
+                        <?php echo $data['tooltip'][$field] ?> 
+                    </div>
+                    <?php endif; ?> 
+                </div>
             </div>
-            
-            <?php if (isset($data['tooltip'][$field])): ?> 
-            <div class="tooltip">
-                <?php echo $data['tooltip'][$field] ?> 
-            </div>
-            <?php endif; ?> 
-        </label>
+        </div>
     <?php endforeach; ?> 
     
     <button class="button blue" type="submit">
