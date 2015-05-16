db11f0229e1c18aeb10cdff682f54e98e5e1cc4a
volter9 <volter925@gmail.com>
2015-05-16 10:36:50 -0700

    Deleted not needed theme

--- themes/mini_framework/html/index.php
+++
@@ -1,5 +0,0 @@
-<?php foreach ($sections as $section): ?> 
-<div class="wrapper" id="<?php echo $section['url'] ?>">
-    <?php echo landing_view_section($section) ?> 
-</div>
-<?php endforeach; ?> 
